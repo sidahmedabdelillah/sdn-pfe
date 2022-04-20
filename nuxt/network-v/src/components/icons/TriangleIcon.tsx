5c81be4ee89a374c9c3007d4b0d70caed455aa97
Abdou_Bentegar <s_bentegar@enst.dz>
2022-04-20 16:33:09 +0100

    fixed triangle icon

--- nuxt/network-v/src/components/icons/TriangleIcon.tsx
+++ nuxt/network-v/src/components/icons/TriangleIcon.tsx
@@ -22,29 +22,29 @@ const TriangleIcon: React.FC = ({ color = "red" , x=0 , y=0} :TriangleIconProps)
         <g id='Danger Triangle'>
           <path
             id='Stroke 3'
-            fill-rule='evenodd'
-            clip-rule='evenodd'
+            fillRule='evenodd'
+            clipRule='evenodd'
             d='M12 21C5.50558 21 2.95666 20.5387 2.54353 18.2033C2.13039 15.8679 4.77383 11.4774 5.58842 10.0285C8.31257 5.18408 10.1637 3 12 3C13.8363 3 15.6874 5.18408 18.4116 10.0285C19.2262 11.4774 21.8696 15.8679 21.4565 18.2033C21.0444 20.5387 18.4944 21 12 21Z'
             stroke={color}
-            stroke-width='1.5'
-            stroke-linecap='round'
-            stroke-linejoin='round'
+            strokeWidth='1.5'
+            strokeLinecap='round'
+            strokeLinejoin='round'
           />
           <path
             id='Stroke 3_2'
             d='M12 8.5V12.395'
             stroke='#130F26'
-            stroke-width='1.5'
-            stroke-linecap='round'
-            stroke-linejoin='round'
+            strokeWidth='1.5'
+            strokeLinecap='round'
+            strokeLinejoin='round'
           />
           <path
             id='Stroke 15'
             d='M11.9957 15.895H12.0047'
             stroke={color}
-            stroke-width='1.5'
-            stroke-linecap='round'
-            stroke-linejoin='round'
+            strokeWidth='1.5'
+            strokeLinecap='round'
+            strokeLinejoin='round'
           />
         </g>
       </g>
