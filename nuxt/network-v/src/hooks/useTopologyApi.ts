5a30ab6d560e847cb7ee4a075c90757c639998d5
Abdou_Bentegar <s_bentegar@enst.dz>
2022-04-22 03:41:31 +0100

    feat: added post server to api hook

--- nuxt/network-v/src/hooks/useTopologyApi.ts
+++ nuxt/network-v/src/hooks/useTopologyApi.ts
@@ -1,16 +1,41 @@
-import { HostInterface, LinkInterface, ServerInterface, SwitchInterface } from '../types/Topology';
-import { useAxiosGet } from './useAxios';
+import {
+  HostInterface,
+  LinkInterface,
+  ServerInterface,
+  SwitchInterface,
+} from '../types/Topology'
+import { useAxiosGet, useAxiosPost } from './useAxios'
 
 export default function useTopologyApi() {
-    const { data: switchData }=  useAxiosGet<SwitchInterface[]>('v1.0/topology/switches' , 10000);
-    const { data : hostData } =  useAxiosGet<HostInterface[]>('v1.0/topology/hosts', 10000);
-    const { data : linksData } = useAxiosGet<LinkInterface[]>('v1.0/topology/links', 10000);
+  const { data: switchData } = useAxiosGet<SwitchInterface[]>(
+    'v1.0/topology/switches',
+    10000
+  )
+  const { data: hostData } = useAxiosGet<HostInterface[]>(
+    'v1.0/topology/hosts',
+    10000
+  )
+  const { data: linksData } = useAxiosGet<LinkInterface[]>(
+    'v1.0/topology/links',
+    10000
+  )
 
-    return { switchData , hostData , linksData}
+  return { switchData, hostData, linksData }
 }
 
 export const useServersApi = () => {
-    const { data: serversData , reload: reloadServers } = useAxiosGet<ServerInterface[]>('v1/loadbalancer/servers' , 1000);
+  const { data: serversData, reload: reloadServers } = useAxiosGet<
+    ServerInterface[]
+  >('v1/loadbalancer/servers', 1000)
+  return { serversData, reloadServers }
+}
 
-    return { serversData , reloadServers }
+export const usePostServerApi = (body: any) => {
+  const { postData: postServerApi } = useAxiosPost(
+    'v1/loadbalancer/servers',
+    body
+  )
+
+  return { postServerApi 
+  }
 }
