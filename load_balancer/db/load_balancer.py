196bfe1e061015eaf0f27929f708ad92b1a961c3
abdou <sidahmedabdelillah>
2022-05-03 13:27:15 +0100

    feat: added virtual_ip to load balancer

--- load_balancer/db/load_balancer.py
+++ load_balancer/db/load_balancer.py
@@ -17,17 +17,17 @@ def get_load_balancers_from_db() -> List[LoadBalancer]:
         )
 
         for row in load_balancers_sql:
-            load_balancers.append(LoadBalancer(row[0] , row[1]))
+            load_balancers.append(LoadBalancer(row[0] , row[1], row[2]))
         
         return load_balancers
     
-def add_loadbalancer_to_db(dpid: str , method : int):
+def add_loadbalancer_to_db(dpid: str , method : int,virtual_ip):
     with connection:
         cursor = connection.cursor()
 
-        sql = "INSERT INTO {}(id,method) VALUES(?,?) ".format(TABLE_NAME)
-        cursor.execute(sql , (dpid,method))
-        return LoadBalancer(dpid,method)
+        sql = "INSERT INTO {}(id,method,virtual_ip) VALUES(?,?,?) ".format(TABLE_NAME)
+        cursor.execute(sql , (dpid,method,virtual_ip))
+        return LoadBalancer(dpid,method,virtual_ip)
 
 def delete_loadbalancer_from_db(dpid:str):
     with connection:
