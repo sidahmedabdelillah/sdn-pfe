c6035cb35541d9ad0cc2e46d69bc69e160d00298
abdou <sidahmedabdelillah>
2022-05-13 13:31:49 +0100

    chores: removed loadbalancer related thing from the switch view component -work in progress-

--- nuxt/network-v/src/components/layout/SideBarMenues/SwitchOverView.tsx
+++ nuxt/network-v/src/components/layout/SideBarMenues/SwitchOverView.tsx
@@ -1,78 +1,123 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Button } from "primereact/button";
 import { Chip } from "primereact/chip";
 
 import { confirmDialog, ConfirmDialog } from "primereact/confirmdialog";
+import { Toast, } from 'primereact/toast'
+
+
 
 import useSideBarStore from "../../../stores/sideBarStore";
 import useTopologyStore from "../../../stores/TopologyStore";
 import { SwitchInterface } from "../../../types/Topology";
 import useLoadBalancersStore from "../../../stores/loadBalancerStore";
-import { useDeleteLoadBalancerApi, usePostLoadBalancerApi } from "../../../hooks/useTopologyApi";
+import {
+  useDeleteLoadBalancerApi,
+  usePostLoadBalancerApi,
+} from "../../../hooks/useTopologyApi";
 import { getLoadBalancers } from "../../../api/loadBalancerApi";
 import useAxiosStore from "../../../stores/axiosStore";
+import { Dialog } from "primereact/dialog";
+import { InputText } from "primereact/inputtext";
+import { validateIpAddress } from "../../../utils/validators/validateIp";
 
 const emptyLoadBalancerToPost = {
-  dpid: ''
+  dpid: "",
+  virtual_ip: ''
 } as {
-  dpid: string
-}
+  dpid: string;
+  virtual_ip: string
+};
 
 const SwitchOverView: React.FC = () => {
-  const [isLoadBalancer , setIsLoadBalancer] = useState(false);
+  const toastRef = useRef<Toast>(null)
+
+  const [isLoadBalancer, setIsLoadBalancer] = useState(false);
   const [switche, setSwitch] = useState<SwitchInterface | null>(null);
 
   const { selectedHost } = useSideBarStore();
   const { switches } = useTopologyStore();
-  
-  const { loadBalancers, getIsLoadBalancer, setLoadBalancers : setStoreLoadBalancers } = useLoadBalancersStore()
-  const [ loadBalancerToPost, setLoadBalancerToPost ] = useState(emptyLoadBalancerToPost)
-  const { postLoadBalancerApi , postLoadBalancerError} = usePostLoadBalancerApi(loadBalancerToPost)
-  const { deletLoadBalancerApi , deleteLoadBalancerError} = useDeleteLoadBalancerApi(selectedHost)
+
+  const [displayDialog, setDisplayDialog] = useState(false);
+  const [inputIp , setInputIp ] = useState('')
+
+  const {
+    loadBalancers,
+    getIsLoadBalancer,
+    setLoadBalancers: setStoreLoadBalancers,
+  } = useLoadBalancersStore();
+  const [loadBalancerToPost, setLoadBalancerToPost] = useState(
+    emptyLoadBalancerToPost
+  );
+  const { postLoadBalancerApi, postLoadBalancerError } =
+    usePostLoadBalancerApi(loadBalancerToPost);
+  const { deletLoadBalancerApi, deleteLoadBalancerError } =
+    useDeleteLoadBalancerApi(selectedHost);
   const { BaseUrl } = useAxiosStore();
 
+  
 
-  const handlePostClick =  () => {
-    confirmDialog({
-      message: 'Are you Sure you want to add load balancing functionality to this switch ?',
-      icon: 'pi pi-exclamation-triangle',
-      accept: () => postLoadBalancer(),
-      reject: () => null
-  });
-  }
+  const showDialog = () => setDisplayDialog(true);
+  const onHide = () => setDisplayDialog(false);
 
-  const postLoadBalancer = async () => {
-    if(loadBalancerToPost === emptyLoadBalancerToPost ) return
+  useEffect(() => {
+    setLoadBalancerToPost((loadBalancerToPost) => ({
+      ...loadBalancerToPost,
+      virtual_ip : inputIp
+    }))
+  }, [inputIp])
+
+  const handlePostClick = () => {
+    if(! validateIpAddress(inputIp)){
+      toastRef.current?.show({severity:'error' ,summary:'invalid Ip Address'})
+      return 
+    }
+
+    console.log(loadBalancerToPost)
+    console.log(inputIp)
     postLoadBalancerApi()
     if(postLoadBalancerError){
       console.log(postLoadBalancerError)
     }
+    setDisplayDialog(false)
+    setTimeout(async () => {
+      const loadBalancers = await getLoadBalancers(BaseUrl);
+      setStoreLoadBalancers(loadBalancers);
+    });
+  };
+
+  const postLoadBalancer = async () => {
+    if (loadBalancerToPost === emptyLoadBalancerToPost) return;
+    postLoadBalancerApi();
+    if (postLoadBalancerError) {
+      console.log(postLoadBalancerError);
+    }
 
     setTimeout(async () => {
-      const loadBalancers = await getLoadBalancers(BaseUrl)
-      setStoreLoadBalancers(loadBalancers)
-    })
-    
-  }
+      const loadBalancers = await getLoadBalancers(BaseUrl);
+      setStoreLoadBalancers(loadBalancers);
+    });
+  };
 
   const deleteServer = () => {
-    deletLoadBalancerApi()
-    if(deleteLoadBalancerError){
-      console.log(deleteLoadBalancerError)
+    deletLoadBalancerApi();
+    if (deleteLoadBalancerError) {
+      console.log(deleteLoadBalancerError);
     }
     setTimeout(async () => {
-      const loadBalancers = await getLoadBalancers(BaseUrl)
-      setStoreLoadBalancers(loadBalancers)
-    })
-  }
-  const handleDeleteClick =  () => {
+      const loadBalancers = await getLoadBalancers(BaseUrl);
+      setStoreLoadBalancers(loadBalancers);
+    });
+  };
+  const handleDeleteClick = () => {
     confirmDialog({
-      message: 'Are you Sure you want remove loadBalancer functionality from this switch ?',
-      icon: 'pi pi-exclamation-triangle',
+      message:
+        "Are you Sure you want remove loadBalancer functionality from this switch ?",
+      icon: "pi pi-exclamation-triangle",
       accept: () => deleteServer(),
-      reject: () => null
-  });
-  }
+      reject: () => null,
+    });
+  };
 
   useEffect(() => {
     console.log(selectedHost);
@@ -80,26 +125,51 @@ const SwitchOverView: React.FC = () => {
     if (foundSwitch) {
       setSwitch(foundSwitch);
     }
-
   }, [selectedHost, switches]);
 
   useEffect(() => {
-    if(selectedHost){
-      setLoadBalancerToPost({dpid: selectedHost})
+    if (selectedHost) {
+      setLoadBalancerToPost({ dpid: selectedHost,virtual_ip:inputIp });
     }
-  } ,[selectedHost])
+  }, [selectedHost]);
 
   useEffect(() => {
-    setIsLoadBalancer(getIsLoadBalancer(selectedHost))
-  },[getIsLoadBalancer, loadBalancers, selectedHost])
+    setIsLoadBalancer(getIsLoadBalancer(selectedHost));
+  }, [getIsLoadBalancer, loadBalancers, selectedHost]);
+
+
+  const footer = (
+    <div>
+      <Button label="Yes" icon="pi pi-check" onClick={handlePostClick} />
+      <Button label="No" icon="pi pi-times" onClick={onHide} />
+    </div>
+  );
+
 
   if (!switche) {
     return <h1>Please Select a host</h1>;
   }
 
   return (
-    <>
+    <>  
+      <Toast ref={toastRef}/>
       <ConfirmDialog />
+      <Dialog
+        visible={displayDialog}
+        header={"Add load balancer"}
+        footer={footer}
+        onHide={onHide}
+      >
+        <h1 className="mb-8">
+          Enter the virtual ip for the cluster connected to this switch
+        </h1>
+        <div className="flex justify-center w-full">
+          <span className="p-float-label w-80">
+            <InputText id="inputtext" className="w-full" value={inputIp} onChange={ e => setInputIp(e.target.value)} />
+            <label htmlFor="inputtext">Ip Address</label>
+          </span>
+        </div>
+      </Dialog>
       <div className="mb-8">
         <h1 className="text-[color:var(--primary-color)] text-xl ">
           {!isLoadBalancer ? (
@@ -126,19 +196,36 @@ const SwitchOverView: React.FC = () => {
       <Chip label="Switch DPID" />
       <p> {switche.dpid} </p>
 
-      <Chip label="Ports" className="mt-4" />
-      {switche.ports.map((port) => (
+      {
+        isLoadBalancer && 
         <>
-          <p>{port.name}</p>
+          <Chip label="Load balancer" />
+          <p> {switche.dpid} </p>
+        </> 
+      }
+
+      {
+        !isLoadBalancer  &&
+        <>
+          <Chip label="Ports" className="mt-4" />
+          {switche.ports.map((port) => (
+              <p key={port.hw_addr} >{port.name}</p>
+          ))}
         </>
-      ))}
+      }
 
       {!isLoadBalancer && (
-        <Button className="mt-4" onClick={handlePostClick}> Add as Load Balancer </Button>
+        <Button className="mt-4" onClick={showDialog}>
+          {" "}
+          Add as Load Balancer{" "}
+        </Button>
       )}
 
       {isLoadBalancer && (
-        <Button className="mt-4" onClick={handleDeleteClick}> Remove server from server cluster </Button>
+        <Button className="mt-4" onClick={handleDeleteClick}>
+          {" "}
+          Remove server from server cluster{" "}
+        </Button>
       )}
     </>
   );
