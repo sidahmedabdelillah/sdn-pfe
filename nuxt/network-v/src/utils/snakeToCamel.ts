b867cdba4fe86e3b9a0107bda4193675b6482a73
abdou <sidahmedabdelillah>
2022-05-13 13:35:05 +0100

    added new utils for string transfromation and ip address validation

---
+++ nuxt/network-v/src/utils/snakeToCamel.ts
@@ -0,0 +1,11 @@
+const snakeToPascal = (string: string) => {
+    return string.split("/")
+      .map(snake => snake.split("_")
+        .map(substr => substr.charAt(0)
+          .toUpperCase() +
+          substr.slice(1))
+        .join(" "))
+      .join("/");
+  };
+
+export default snakeToPascal
